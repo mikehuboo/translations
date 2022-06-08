9ca2005aef3e749d50005d2b5ad1317329e7547a
Mike Karthauser <mike.karthauser@huboo.com>
2022-06-08 18:09:23 +0100

    moving up a directory

--- src/Providers/TranslationServiceProvider.php
+++ src/Providers/TranslationServiceProvider.php
@@ -49,7 +49,7 @@ class TranslationServiceProvider extends BaseTranslationServiceProvider
      */
     public function boot(): void
     {
-        include dirname(__FILE__) . '../routes.php';
+        include realpath(__DIR__ . '/../routes.php');
     }
 
     protected function registerLoader()
