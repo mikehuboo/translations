6fa4a91b3c74b60384d47ac54632a0729cbf93a7
Mike Karthauser <mike.karthauser@huboo.com>
2022-06-09 10:24:30 +0100

    adds config for webhook client

--- src/Providers/TranslationServiceProvider.php
+++ src/Providers/TranslationServiceProvider.php
@@ -45,6 +45,18 @@ class TranslationServiceProvider extends BaseTranslationServiceProvider
     }
 
     /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/webhook-client.php' => config_path('webhook-client'),
+        ]);
+    }
+
+    /**
      * @return void
      */
     public function boot(): void
